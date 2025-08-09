89150a4993c79159dc09e8c34a1ef47031525674
Ad Soyad <mustafazfrr@gmail.com>
2025-08-10 00:49:13 +0300

    bug fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -14,31 +13,29 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
+    kotlinOptions { jvmTarget = JavaVersion.VERSION_11.toString() }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.denemeye_devam"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        // Gradle property > Env var sırası ile oku
+        val mapsKey = providers.gradleProperty("MAPS_API_KEY").orNull
+            ?: providers.environmentVariable("MAPS_API_KEY").orNull
+            ?: ""
+
+        // Manifest placeholder'ını bas
+        manifestPlaceholders["MAPS_API_KEY"] = mapsKey
+        // İstersen zorunlu kıl:
+        // check(mapsKey.isNotBlank()) { "MAPS_API_KEY tanımlı değil" }
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
+        release { signingConfig = signingConfigs.getByName("debug") }
     }
 }
 
-flutter {
-    source = "../.."
-}
+flutter { source = "../.." }
